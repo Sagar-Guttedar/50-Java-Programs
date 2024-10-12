7dc49429b024d725a01fdc377f0ae5a16639cdc9
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-10-12 02:30:03 -0400

    Add files via upload

---
+++ Java-programs/Max.java
@@ -0,0 +1,17 @@
+
+// Maximum of three numbers
+
+
+public class Max {
+    public static void main(String[] args) {
+        int a=10, b=15, c=3;
+
+        if(a>b && c>c){
+            System.out.println("Maximum No is : "+a);
+        }else if (b>a && b>c){
+            System.out.println("Maximum No is : "+b);
+        }else{
+             System.out.println("Maximum No is : "+c);
+        }
+    }
+}
