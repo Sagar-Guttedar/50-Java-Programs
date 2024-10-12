9546974c3693f9b2795827d4e36896304fcd6260
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-10-12 02:34:05 -0400

    Add files via upload

---
+++ Java-programs/PatternProgram1.java
@@ -0,0 +1,12 @@
+// Simple pattern program
+
+public class PatternProgram1 {
+    public static void main(String[] args) {
+        for(int i=1; i<=6; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
