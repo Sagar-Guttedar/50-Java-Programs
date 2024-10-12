508c1cbdb4aaabe760eeb225fad5b95f5ba2dab6
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-10-12 02:38:40 -0400

    Add files via upload

---
+++ Java-programs/PatternProgram2.java
@@ -0,0 +1,12 @@
+// Reverse Stars Patter Program
+
+public class PatternProgram2 {
+    public static void main(String[] args) {
+        for(int i=1; i<=3; i++){
+            for (int j=3; j>=i; j--){
+                System.out.print("*");
+            }
+            System.out.println("");;
+        }
+    }
+}
