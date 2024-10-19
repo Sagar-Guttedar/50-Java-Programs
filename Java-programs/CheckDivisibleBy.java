d15b68378d27ee79413574dd02fd34060e12d997
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-10-19 01:16:25 -0400

    Add files via upload

---
+++ Java-programs/CheckDivisibleBy.java
@@ -0,0 +1,12 @@
+// To check Number is Divisible eby 5 or not
+
+public class CheckDivisibleBy{
+    public static void main(String[] args) {
+        int num = 15;
+        if (num % 5==0){
+            System.out.println("Divisible");
+        }else{
+            System.out.println("Not Divisible");
+        }
+    }
+}
