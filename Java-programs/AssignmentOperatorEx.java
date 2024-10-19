a846937987d543d8c04f68be08c9df4ded839516
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-10-19 01:31:15 -0400

    Add files via upload

---
+++ Java-programs/AssignmentOperatorEx.java
@@ -0,0 +1,13 @@
+
+
+// Assignment operators += , -= , *= , /= , %=
+
+
+public class AssignmentOperatorEx{
+    public static void main(String[] args) {
+        int i=10;
+        System.out.println("Value of i : "+i);
+        i -= 5;;
+        System.out.println("Value of i******* : "+i);
+    }
+}
