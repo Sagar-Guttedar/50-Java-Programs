91e1ea1a8ece3de6d11a7b168d020571903d695c
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-10-19 01:21:40 -0400

    Add files via upload

---
+++ Java-programs/PositiveNegetive.java
@@ -0,0 +1,15 @@
+// To check given number is positive or negetive or zero
+
+
+public class PositiveNegetive{
+    public static void main(String[] args) {
+        int num = 10;
+        if (num>0){
+            System.out.println("positive ");
+        }else if(num<0){
+            System.out.println("Negetive");
+        }else{
+            System.out.println("Zero");
+        }
+    }
+}
