f453f91a4db0936730ad7d257132780bcc65fc87
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-10-19 01:28:03 -0400

    Add files via upload

---
+++ Java-programs/ArithmeticEx.java
@@ -0,0 +1,15 @@
+// Arithmetic operators :  + - * / %
+
+
+public class ArithmeticEx {
+
+    public static void main(String[] args) {
+        for (int i=1; i<=6;i++){
+            if (i%2==0){
+                System.out.println("Even Number");
+            }else{
+                System.out.println("Odd Number");
+            }
+        }
+    }
+}
