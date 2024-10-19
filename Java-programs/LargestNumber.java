108553152c22bb05f3e00f37e27f7a2bdaa4827a
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-10-19 01:17:44 -0400

    Add files via upload

---
+++ Java-programs/LargestNumber.java
@@ -0,0 +1,13 @@
+
+// To find Maximum out of two number
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        int num1=14, num2 = 15;
+        if(num1>num2){
+            System.out.println(num1);
+        }else{
+            System.out.println(num2);
+        }
+    }
+}
