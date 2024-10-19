c5d8acd511c2e3b3675f81cca74fd08406392049
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-10-19 01:25:26 -0400

    Add files via upload

---
+++ Java-programs/Vowels.java
@@ -0,0 +1,14 @@
+
+// Write a Java Program To Check Given Character is Vowel or Not | Java Boolean Operator
+
+public class Vowels{
+    public static void main(String[] args) {
+        char c = 'e';
+
+        if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+            System.out.println("Vowel");
+        }else{
+            System.out.println("Not vowel");
+        }
+    }
+}
