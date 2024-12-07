73cc66ead974b7c04c8e96e61efbf46bbd76df13
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-12-07 00:52:23 -0500

    Add files via upload

---
+++ Java-programs/AverageOfTwoNumbers.java
@@ -0,0 +1,13 @@
+
+
+// Write a program Average Of two numbers.
+
+public class AverageOfTwoNumbers{
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=100;
+
+        double avg=(num1+num2)/2;
+        System.out.println(avg);
+    }
+}
