46015f48b6c6b71a9f85a1db87fff12e290020de
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-12-07 00:48:51 -0500

    Add files via upload

---
+++ Java-programs/PrimeNumberEx.java
@@ -0,0 +1,13 @@
+
+// Write a program Prime number
+
+public class PrimeNumberEx {
+    public static void main(String[] args) {
+        int num=8;
+        if (num<=1){
+            System.out.println("Prime number");
+        }else{
+            System.out.println("Not Prime Number.");
+        }
+    }
+}
