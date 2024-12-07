d56b8cf90522649d624ba1f82f6949807c9ecd35
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-12-07 01:27:11 -0500

    Add files via upload

---
+++ Java-programs/ExplicitCastingExample.java
@@ -0,0 +1,12 @@
+
+
+// Write a program to Explicitly Casting example.
+
+public class ExplicitCastingExample {
+    public static void main(String[] args) {
+        double d = 100.99;
+        int num = (int) d;  // double to int
+        System.out.println("Double value: " + d);
+        System.out.println("Integer value: " + num);
+    }
+}
