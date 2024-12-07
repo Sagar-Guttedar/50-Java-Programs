6e79f5eef6da11b2b59736d92f4aa060414963fb
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-12-07 00:56:06 -0500

    Add files via upload

---
+++ Java-programs/FinndLengthOfArray.java
@@ -0,0 +1,11 @@
+
+
+// Write a Program find length of an given array.
+
+public class FinndLengthOfArray {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40};
+
+        System.out.println(numbers.length);
+    }
+}
