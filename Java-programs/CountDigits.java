51e2b974ce3b2def3cca2653b10c76646b9159da
Sagar Guttedar <126396989+Sagar-Guttedar@users.noreply.github.com>
2024-12-07 01:17:52 -0500

    Add files via upload

---
+++ Java-programs/CountDigits.java
@@ -0,0 +1,12 @@
+
+
+// Count the Number of Digits in aNumber
+
+
+public class CountDigits {
+    public static void main(String[] args) {
+        int num=123456;
+        int count=String.valueOf(num).length();
+        System.out.println(count);
+    }
+}
